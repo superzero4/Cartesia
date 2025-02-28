17c322473ce7d13f75f852242a8bb33e644c97f5
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-02-28 15:28:59 -0500

    Created Extension class for point, to convert to UnityVector3

---
+++ Assets/Structures Geométriques/Extensions/PointExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Structures_Geométriques.Extensions
+{
+    public static class PointExtensions
+    {
+        public static Vector3 ToVector3(this Point point)
+        {
+            return new Vector3(point.x, point.y, point.z);
+        }
+    }
+}
