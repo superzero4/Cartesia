84f20dd7a3f3ca6daffebc38ec339d39a474f3bc
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-02-28 15:30:18 -0500

    Created some Runner scripts to run in scene using a geomerty list contained in a scriptable (temporary for testing now)

---
+++ Assets/Scripts/Geometries.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using NUnit.Framework;
+
+[CreateAssetMenu(fileName = "Geometries", menuName = "Geometries")]
+public class Geometries : ScriptableObject
+{
+    [Header("Data")] [SerializeField] private List<Point> _points;
+
+    [SerializeField, Tooltip("Index of points in the list")]
+    private Vector2Int[] _lines;
+
+    [SerializeField] private List<Indexes> _polygons;
+
+    [Serializable]
+    private class Indexes
+    {
+        public int[] indexes;
+    }
+
+    private List<Segment> _segments = null;
+    private List<Polygone> _polygones = null;
+
+    public List<Point> Points => _points;
+
+    public void ResetShapes()
+    {
+        _segments = null;
+        _polygones = null;
+    }
+
+    //TODO add some kind of dirty on the _lines list to ensure the segment list are updated properly
+    public List<Segment> Segments => _segments == null || _segments.Count == 0 ? GenerateSegments() : _segments;
+    public List<Polygone> Polygones => _polygones == null || _polygones.Count == 0 ? GeneratePolygones() : _polygones;
+
+    private List<Polygone> GeneratePolygones()
+    {
+        return _polygones = _polygons
+            .Select(poly => new Polygone(
+                poly.indexes
+                    .Where(index => isIndexValid(index))
+                    .Select(index => _points[index]).ToList()))
+            .ToList();
+    }
+
+    private bool isIndexValid(int index) => index >= 0 && index < _points.Count;
+
+    private List<Segment> GenerateSegments()
+    {
+        return _segments = _lines
+            .Where(point => point.x != point.y && isIndexValid(point.x) && isIndexValid(point.y))
+            .Select(point => new Segment(_points[point.x], _points[point.y]))
+            .ToList();
+    }
+}
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Renderers;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    [SerializeField] private Geometries _geometries;
+
+    [SerializeField] private PointRenderer _pointPrefab;
+    [SerializeField] private SegmentRenderer _segmentPrefab;
+    [SerializeField] private PolygonRenderer _polygonPrefab;
+    [SerializeField] private List<PointRenderer> _pointRenderers;
+    [SerializeField] private List<SegmentRenderer> _segmentRenderers;
+    [SerializeField] private List<PolygonRenderer> _polygonRenderers;
+
+    private void Start()
+    {
+        _geometries.ResetShapes();
+    }
+
+    private void Update()
+    {
+        int i = 0;
+        for (; i < _geometries.Points.Count; i++)
+        {
+            if (i >= _pointRenderers.Count)
+            {
+                var rend = Instantiate(_pointPrefab, transform);
+                _pointRenderers.Add(rend);
+            }
+            _pointRenderers[i].ToggleVisibility(true);
+            _pointRenderers[i].SetView(_geometries.Points[i]);
+        }
+        for(; i < _pointRenderers.Count; i++)
+        {
+            _pointRenderers[i].ToggleVisibility(false);
+        }
+        var segments = _geometries.Segments;
+        i = 0;
+        for (; i < segments.Count; i++)
+        {
+            if (i >= _segmentRenderers.Count)
+            {
+                var rend = Instantiate(_segmentPrefab, transform);
+                _segmentRenderers.Add(rend);
+            }
+            _segmentRenderers[i].ToggleVisibility(true);
+            _segmentRenderers[i].SetView(segments[i]);
+        }
+        for(; i < _segmentRenderers.Count; i++)
+        {
+            _segmentRenderers[i].ToggleVisibility(false);
+        }
+        var polygons = _geometries.Polygones;
+        i = 0;
+        for (; i < polygons.Count; i++)
+        {
+            if (i >= _polygonRenderers.Count)
+            {
+                var rend = Instantiate(_polygonPrefab, transform);
+                _polygonRenderers.Add(rend);
+            }
+            _polygonRenderers[i].ToggleVisibility(true);
+            _polygonRenderers[i].SetView(polygons[i]);
+        }
+    }
+}
