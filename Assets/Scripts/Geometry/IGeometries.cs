4fd57d61a1dc137934d2e1122797cd6c3c5b52f3
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-03-18 19:40:47 -0400

    Enhanced IGeometries interface to work properly as a supertype of current impl

--- Assets/Scripts/Geometry/IGeometries.cs
+++ Assets/Scripts/Geometry/IGeometries.cs
@@ -1,8 +1,14 @@
 ﻿using System.Collections.Generic;
+using Structures_Geométriques;
 
 public interface IGeometries
 {
     IEnumerable<Point> Points { get; }
     IEnumerable<Segment> Segments { get; }
     IEnumerable<Polygone> Polygones { get; }
+    IEnumerable<Polyedre> Polyedres { get; }
+    int PolygonesCount { get; }
+    int PointsCount { get; }
+    void AddPoint(Point point);
+    void AddIndex(int geomPolygonesCount, int geomPointsCount);
 }
