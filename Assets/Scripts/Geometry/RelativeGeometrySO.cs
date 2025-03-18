bf982839f6643007c3af5fee3ebdf63c8e3f5990
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-03-18 19:41:27 -0400

    Created a simple SO cotnaint a relative geom (than we can copy construct to a runtime var
    Use to stor intial configuration

---
+++ Assets/Scripts/Geometry/RelativeGeometrySO.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Geometry
+{
+    [CreateAssetMenu(fileName = "RelativeGeometry", menuName = "Geometry/RelativeGeometry", order = 0)]
+    public class RelativeGeometrySO : ScriptableObject
+    {
+        [SerializeField] private RelativeGeometry _relativeGeometry;
+
+        public RelativeGeometry RelativeGeometry => _relativeGeometry;
+    }
+}
