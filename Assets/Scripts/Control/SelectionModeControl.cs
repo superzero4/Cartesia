1451a1083d9300decb3e6f9c0a41fdadffdd2e01
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-03-19 00:17:18 -0400

    Create SelectionMode script to control the mode from keyboard

---
+++ Assets/Scripts/Control/SelectionModeControl.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Control
+{
+    public enum SelectionMode
+    {
+        None = 0,
+        Point = 1,
+        Line = 2,
+        Face = 3,
+        Object = 4
+    }
+
+    public class SelectionModeControl : MonoBehaviour
+    {
+        private SelectionMode _selectionMode = SelectionMode.None;
+        public UnityEvent<string> OnChangeMode;
+        public SelectionMode SelectionMode => _selectionMode;
+
+        private void Update()
+        {
+            SelectionMode newMode = SelectionMode.None;
+            if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Z))
+            {
+                newMode = SelectionMode.Point;
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.X))
+            {
+                newMode = SelectionMode.Line;
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.C))
+            {
+                newMode = SelectionMode.Face;
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.V))
+            {
+                newMode = SelectionMode.Object;
+            }
+
+            if (newMode != SelectionMode.None && newMode != _selectionMode)
+            {
+                _selectionMode = newMode;
+                OnChangeMode.Invoke(_selectionMode.ToString());
+            }
+        }
+    }
+}
