3824f7fd34beb27713fa5347295d9ecb2ba16384
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-03-18 23:00:10 -0400

    Create a simple flyer to move and trigger the manipulator

---
+++ Assets/Scripts/Tools/SimpleFlyer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Tools
+{
+    public class SimpleFlyer : MonoBehaviour
+    {
+        [SerializeField] private Manipulator _manipulator;
+        [SerializeField,Range(0.001f, 10f)] private float _speed = 1f;
+        [SerializeField,Range(0.001f, 100f)] private float _rotationSpeed = 1f;
+        public void Update()
+        {
+            var translation = Input.GetAxis("Vertical") * _speed;
+            var rotation = Input.GetAxis("Horizontal") * _rotationSpeed;
+            _manipulator.externalFakeInput = Input.GetKey(KeyCode.Space);
+            translation *= Time.deltaTime;
+            rotation *= Time.deltaTime;
+            transform.Translate(0, 0, translation);
+            transform.Rotate(0, rotation, 0);
+        }
+    }
+}
