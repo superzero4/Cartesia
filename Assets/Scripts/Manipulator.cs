3da3ad7d900d66641150ef1b836b8e5aa894baca
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-03-18 22:59:57 -0400

    Make manipulator work with externally trigger

--- Assets/Scripts/Manipulator.cs
+++ Assets/Scripts/Manipulator.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using Renderers;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public enum SelectionMode
 {
@@ -22,8 +23,10 @@ public class Manipulator : MonoBehaviour
     private PolyedreRenderer _object;
 
     private UnityEngine.XR.InputDevice device;
+
     //Ajout d'une variable SelectionMode
     [SerializeField] private SelectionMode _selectionMode = SelectionMode.Object;
+
     //Ajout Liste des points à selectionner pour bouger la ligne et une face
     private List<PointRenderer> _selectedPointsSegment = new List<PointRenderer>();
     private List<PolygonRenderer> _selectedPointsFace = new List<PolygonRenderer>();
@@ -31,6 +34,7 @@ public class Manipulator : MonoBehaviour
 
     private void Start()
     {
+        device = default;
         _geom = _main.RuntimeGeometry;
         var devices = new List<UnityEngine.XR.InputDevice>();
         UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.LeftHand, devices);
@@ -45,14 +49,14 @@ public class Manipulator : MonoBehaviour
             Debug.Log("Found more than one left hand!");
         }
     }
+
     private bool _release;
-
-
+    [NonSerialized] public bool externalFakeInput;
 
     private void Update()
     {
         Debug.Log("Point : " + (_point != null));
-        if (Input.GetKey(KeyCode.Space) || VRInput())
+        if (externalFakeInput || VRInput())
         {
             var pos = transform.position;
             switch (_selectionMode)
@@ -62,34 +66,38 @@ public class Manipulator : MonoBehaviour
                     {
                         _point.Data.Set(pos.x, pos.y, pos.z);
                     }
+
                     break;
                 case SelectionMode.Line:
                     // Logique de manipulation d'une ligne
                     //On selectionne d'abord la ligne et ses points
-                    if (_line !=null)
+                    if (_line != null)
                     {
                         _selectedPointsSegment = GetLinePoints();
                         //On deplace la ligne
                         MoveLine(pos);
                     }
+
                     break;
 
                 case SelectionMode.Face:
                     // Logique de manipulation d'une face
                     // Selection des points de la face à manipuler
-                    if (_face!=null)
+                    if (_face != null)
                     {
                         //_selectedPoints = GetFacePoints();
                         // On deplace la face
                         //MoveSelectedPoints(pos);
                     }
+
                     break;
 
                 case SelectionMode.Object:
-                    if (_object!=null)
+                    if (_object != null)
                     {
                         MoveObject(pos);
                     }
+
                     break;
             }
         }
@@ -119,24 +127,24 @@ public class Manipulator : MonoBehaviour
     }
 
     // Ajout fonction pour récupérer les points d'une ligne
-     private List<PointRenderer> GetLinePoints()
+    private List<PointRenderer> GetLinePoints()
     {
         List<PointRenderer> linePoints = new List<PointRenderer>();
 
         linePoints.Add(_line._start);
         linePoints.Add(_line._end);
-      
+
         return linePoints;
     }
-    
+
 
     //FIN DU CODE QUE J AI AJOUTE
 
 
     private bool VRInput()
     {
-        bool triggerValue;
-        if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerValue) && triggerValue)
+        bool triggerValue = false;
+        if (device.isValid && device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerValue) && triggerValue)
         {
             return true;
         }
@@ -179,5 +187,4 @@ public class Manipulator : MonoBehaviour
         _line = null;
         _object = null;
     }
-
 }
